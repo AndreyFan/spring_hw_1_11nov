c9963a873709d1cf7c711e70049a92493399fd4a
AndreyFan <130384653+AndreyFan@users.noreply.github.com>
2024-11-13 00:36:53 +0100

    spring_hw_1_11nov_Fandieiev

---
+++ onlineshop/controller/CartController.java
@@ -0,0 +1,17 @@
+package de.telran.onlineshop.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+    @RestController
+    @RequestMapping("/cart")
+    public class CartController {
+
+        @GetMapping
+        public String getCart() {
+            return "Привет, я контроллер - CartController";
+        }
+    }
+
---
+++ onlineshop/controller/OrderItemsController.java
@@ -0,0 +1,15 @@
+package de.telran.onlineshop.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/orderItems")
+public class OrderItemsController {
+
+    @GetMapping
+    public String getOrderItems() {
+        return "Привет, я контроллер - OrderItemsController";
+    }
+}
